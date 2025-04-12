bb00e0a30de7fc87bfaaaed443b2f22fd6991b8e
Roushani06 <roushani77100@gmail.com>
2025-04-13 00:30:40 +0530

    backend part

--- server/routes/lawyerRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createLawyer, getAllLawyers } from "../controllers/lawyerController.js";
-
-const lawyerRouter = express.Router();
-
-lawyerRouter.post("/", createLawyer);      // POST /api/lawyers
-lawyerRouter.get("/", getAllLawyers);      // GET /api/lawyers
-
-export default lawyerRouter;
